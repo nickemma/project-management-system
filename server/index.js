2e974bec7f18ba5a8c7900acc43af090dc240ebb
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-02-21 09:09:48 +0100

    Added graphql to index

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ dotenv.config();
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema/schema.js';
 import morgan from 'morgan';
 
 const app = express();
@@ -13,6 +15,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
 app.use(morgan('dev'));
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development' ? true : false,
+  })
+);
 
 // ========= Routes ===========
 
