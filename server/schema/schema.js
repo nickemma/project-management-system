28ec0632bd11fda7366f9b266301ab50d038ae73
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-02-22 16:46:57 +0100

    Added the project data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,11 +5,23 @@ import {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
+  GraphQLList,
 } from 'graphql';
+
+//  Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+  }),
+});
 
 //  Client Type
 const ClientType = new GraphQLObjectType({
-  name: 'Project',
+  name: 'Client',
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
@@ -21,6 +33,27 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // code to get data from db / other source
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
